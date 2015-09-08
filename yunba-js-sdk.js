066ac48693dcef59fe9992e2123c221a4ff79de0
Cao Jiafeng <funfriendcjf@gmail.com>
2015-09-08 19:57:33 +0800

    the Array helper functions(or polyfills) is never used

--- yunba-js-sdk.js
+++ yunba-js-sdk.js
@@ -102,28 +102,6 @@ var Yunba = (function () {
         }
     };
 
-    Array.prototype.contain = function (val) {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i] == val) {
-                return true;
-            }
-        }
-        return false;
-    };
-
-    Array.prototype.indexOf = function (val) {
-        for (var i = 0; i < this.length; i++) {
-            if (this[i] == val) return i;
-        }
-        return -1;
-    };
-
-    Array.prototype.remove = function (val) {
-        var index = this.indexOf(val);
-        if (index > -1) {
-            this.splice(index, 1);
-        }
-    };
     function Yunba(setup) {
         setup = setup || {};
         this.server = setup['server'] || DEF_SERVER;
